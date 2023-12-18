806702726cb36dfeb3b32b1b1bb710e7c9f0b4de
SpawnID0000 <142523337+SpawnID0000@users.noreply.github.com>
2023-12-18 13:42:03 -0800

    Update decrypt_concat_script.py
    Adding multiprocessing capability for faster operation and also switched encryption from GnuPG to Python's built-in cryptography libraries

--- decrypt_concat_script.py
+++ decrypt_concat_script.py
@@ -1,83 +1,103 @@
 import os
-import subprocess
 import argparse
 import zipfile
 import logging
 import time
+from multiprocessing import Pool
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
 from pathlib import Path
 
-# Initialize logging
-script_name = os.path.splitext(os.path.basename(__file__))[0]
-log_file = f"{script_name}_log.txt"
-logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# AES decryption function
+def aes_decrypt(encrypted_data, passphrase):
+    backend = default_backend()
+    key = passphrase.ljust(32)[:32].encode()
+    iv = encrypted_data[:16]
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=backend)
+    decryptor = cipher.decryptor()
 
-# Start time for duration calculation
-start_time = time.time()
+    unpadder = padding.PKCS7(128).unpadder()
+    decrypted_padded_data = decryptor.update(encrypted_data[16:]) + decryptor.finalize()
+    decrypted_data = unpadder.update(decrypted_padded_data) + unpadder.finalize()
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Decrypt and concatenate audio files. \n\nUsage: python3 decrypt_concat_script.py path/to/encrypted path/to/output',
-                                 formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('input_dir', type=str, help='Path to the directory with encrypted files')
-parser.add_argument('output_dir', type=str, help='Path to the output directory')
-args = parser.parse_args()
+    return decrypted_data
 
-# Decrypt and concatenate function
-def decrypt_and_concatenate(input_dir, output_dir):
-    file_count = 0
-    for root, dirs, files in os.walk(input_dir):
+# Function to process a single ZIP package
+def process_zip_package(zip_info):
+    zip_path, input_root, output_root = zip_info
+    extracted_dir = os.path.join(os.path.dirname(zip_path), Path(zip_path).stem)
+
+    try:
+        os.makedirs(extracted_dir, exist_ok=True)
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extracted_dir)
+
+        decrypted_files = {}
+        for aes_file in sorted(os.listdir(extracted_dir)):
+            if aes_file.endswith('.aes'):
+                full_path = os.path.join(extracted_dir, aes_file)
+                parts = Path(aes_file).stem.split('_')
+                passphrase = '_'.join(parts[:-2])  # Remove '_part_xx' and '.ext'
+                original_ext = '.' + aes_file.split('.')[-2]  # Get the original extension
+
+                with open(full_path, 'rb') as f:
+                    encrypted_data = f.read()
+                decrypted_data = aes_decrypt(encrypted_data, passphrase)
+
+                if passphrase not in decrypted_files:
+                    decrypted_files[passphrase] = b''
+                decrypted_files[passphrase] += decrypted_data
+
+                os.remove(full_path)
+
+        for passphrase, data in decrypted_files.items():
+            relative_dir = Path(zip_path).parent.relative_to(input_root)
+            output_dir = Path(output_root, relative_dir)
+            os.makedirs(output_dir, exist_ok=True)
+
+            output_file_path = output_dir / f"{passphrase}{original_ext}"
+            with open(output_file_path, 'wb') as fout:
+                fout.write(data)
+
+        os.rmdir(extracted_dir)
+        print(f"Finished processing ZIP package: {zip_path}")
+        return f"Processed ZIP package: {zip_path}"
+
+    except Exception as e:
+        return f"Error processing package {zip_path}: {e}"
+
+def main():
+    script_name = os.path.splitext(os.path.basename(__file__))[0]
+    log_file = f"{script_name}_log.txt"
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    start_time = time.time()
+
+    parser = argparse.ArgumentParser(description='Decrypt and concatenate audio files. \n\nUsage: python3 decrypt_concat_script.py path/to/encrypted path/to/output',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('input_dir', type=str, help='Path to the directory with encrypted files')
+    parser.add_argument('output_dir', type=str, help='Path to the output directory')
+    args = parser.parse_args()
+
+    zip_packages_to_process = []
+    for root, dirs, files in os.walk(args.input_dir):
         for file in files:
-            if file.startswith('.') or not file.endswith('.zip'):  # Skip hidden files and non-ZIP files
-                continue
+            if file.endswith('.zip'):
+                zip_packages_to_process.append((os.path.join(root, file), args.input_dir, args.output_dir))
 
-            try:
-                # Unzip the package
-                zip_path = os.path.join(root, file)
-                print(f"Extracting ZIP package: {zip_path}")
-                with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-                    zip_ref.extractall(root)
+    print("Processing ZIP packages...")
+    with Pool() as pool:
+        results = pool.map(process_zip_package, zip_packages_to_process)
 
-                # Process the unzipped files
-                extracted_files = [filename for filename in os.listdir(root) if filename.endswith('.gpg')]
-                for part_file in extracted_files:
-                    full_path = os.path.join(root, part_file)
-                    passphrase = os.path.basename(part_file).split('_part_')[0]
-                    decrypted_file_path = full_path.rstrip('.gpg')
+    # # Logging results
+    # for result in results:
+    #     logging.info(result)  # Comment out or remove this line to suppress detailed logging
+    #     print(result)
 
-                    # Define the output file path, preserving the directory structure
-                    relative_dir = os.path.relpath(root, input_dir)
-                    output_subdir = os.path.join(output_dir, relative_dir)
-                    os.makedirs(output_subdir, exist_ok=True)
-                    output_file_path = os.path.join(output_subdir, passphrase + ".opus")  # Default to '.opus'
+    total_duration = time.time() - start_time
+    logging.info(f"Total script execution duration: {total_duration:.2f} seconds")
+    print("Decryption and concatenation complete!")
 
-                    # Decrypt file
-                    gpg_path = "/usr/local/bin/gpg"
-                    print(f"Decrypting file: {part_file} using passphrase: {passphrase}")
-                    subprocess.run([gpg_path, "-d", "--output", decrypted_file_path, "--batch", "--yes", "--passphrase", passphrase, full_path])
-
-                    # Concatenate file
-                    with open(output_file_path, 'ab') as fout:
-                        with open(decrypted_file_path, 'rb') as fin:
-                            fout.write(fin.read())
-
-                    # Cleanup decrypted chunk
-                    os.remove(decrypted_file_path)
-
-                # Delete the unzipped .gpg files after processing
-                for part_file in extracted_files:
-                    os.remove(os.path.join(root, part_file))
-
-                file_count += 1
-                print(f"Processed ZIP package: {zip_path}")
-                #logging.info(f"Processed ZIP package: {zip_path}")
-
-            except Exception as e:
-                logging.error(f"Error processing package {zip_path}: {e}")
-
-    return file_count
-
-# Running the function and logging results
-file_count = decrypt_and_concatenate(args.input_dir, args.output_dir)
-total_duration = time.time() - start_time
-logging.info(f"Total files processed: {file_count}")
-logging.info(f"Total script execution duration: {total_duration:.2f} seconds")
-print("Decryption and concatenation complete!")
+if __name__ == "__main__":
+    main()
